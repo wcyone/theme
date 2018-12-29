f49c8687d6afd02ca479aebc210ecf012b9c7734
wcyone <421035835@qq.com>
2018-12-29 16:21:42 +0800

    test

---
+++ src/main/java/com.yinhai/dao/CC.java
@@ -0,0 +1,12 @@
+package com.yinhai.dao;
+
+/**
+ * @author wcy
+ * @date 2018/12/29 16:20
+ * @Description:
+ */
+public class CC {
+    public static void main(String[] args){
+        System.out.print("1");
+    }
+}
